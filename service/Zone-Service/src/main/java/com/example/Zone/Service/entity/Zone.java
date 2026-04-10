751bec68232b3c44fd8e0cc6385c4766a88241dd
Prasadi Durage <ps7032962@gamail.com>
2026-04-10 12:02:04 +0530

    initialize Sensor Service with Maven wrapper, configuration files, and application structure

---
+++ service/Zone-Service/src/main/java/com/example/Zone/Service/entity/Zone.java
@@ -0,0 +1,40 @@
+package com.example.Zone.Service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Builder
+@Table(name = "zones")
+public class Zone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double minTemp;
+
+    @Column(nullable = false)
+    private Double maxTemp;
+
+    private String deviceId;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
